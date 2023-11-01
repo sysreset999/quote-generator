21a4a7000232e8a31f9c5e316d71f889ea550414
Uday Prakash Gupta <128396030+sysreset999@users.noreply.github.com>
2023-11-01 08:25:38 +0000

    made a little responsive, phew!

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const api = 'https://api.quotable.io/random'
+// Made by: Uday Prakash Gupta
+// github: @sysreset999
+// website:- https://udayprakashgupta.me
+
+const api = 'https://api.quotable.io/quotes/random?maxLength=70'
 
 function getQuote(){
     fetch(api).then(res =>{
     return res.json()
     .then(data =>{
-        console.log(data)
-        const authName =  data.author;
-        document.getElementById('qt-area').innerHTML = data.content + ` -&nbsp`+ `<lol>${authName}</lol>`;
+        console.log(data[0])
+        const authName =  data[0].author;
+        document.getElementById('qt-area').innerHTML = data[0].content + `<lol>- ${authName}</lol>`;
     })
 })
 }
